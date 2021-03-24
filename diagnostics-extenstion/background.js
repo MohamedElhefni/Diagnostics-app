e1496ce1f45006e0f37a460e1e9a5b987ad77598
MohamedElhefni <elhefnymohamed446@gmail.com>
2021-03-24 16:19:04 +0200

    add memory usage percentage feature to extension

--- diagnostics-extenstion/background.js
+++ diagnostics-extenstion/background.js
@@ -49,55 +49,25 @@ chrome.runtime.onConnect.addListener(function (port) {
       });
     }
 
-    // if (msg.question == "getCpuUsage") {
-
-    // }
-
-    // setInterval(() => {
-    //    chrome.system.cpu.getInfo(function(info){
-    //       port.postMessage(info)
-    //    });
-
-    // }, 1000)
-    //    if (msg.joke == "Knock knock")
-    //     {
-    //       port.postMessage({question: "Who's there?"});
-    //     }
-    //   else if (msg.answer == "Madame")
-    //     port.postMessage({question: "Madame who?"});
-    //   else if (msg.answer == "Madame... Bovary")
-    //     port.postMessage({question: "I don't get it."});
+   
   });
 });
 
-// chrome.tabs.query({title: "Dignostics App"}, function(tabs) {
-//    chrome.tabs.sendMessage(tabs[0].id, {message: "hello"}, function(response) {
-//      console.log(response);
-//    });
-//  });
-
-// chrome.runtime.sendMessage("hello world from ext")
-
-// chrome.runtime.onMessage.addListener(function(response, sender, sendResponse) {
-//    alert(response)
-// })
 
 chrome.system.cpu.getInfo(function (info) {
   // console.log(JSON.stringify(info));
   console.log(info);
 });
 
-chrome.system.memory.getInfo(function (info) {
-  // console.log(JSON.stringify(info));
-  console.log(info);
-});
+// setInterval(() => {
+//   chrome.system.memory.getInfo(function (info) {
+//     // console.log(JSON.stringify(info));
+//     console.log(info.availableCapacity);
+//   });
+// }, 1000)
 
 chrome.system.storage.getInfo(function (info) {
   // console.log(JSON.stringify(info));
   console.log(info);
 });
 
-// chrome.system.display.getInfo(function(info){
-//    // console.log(JSON.stringify(info));
-//    console.log(info)
-// });
--- diagnostics-extenstion/popup.js
+++ diagnostics-extenstion/popup.js
@@ -1,26 +1,32 @@
 let cpuInfo = document.getElementById("cpu-information");
 let processorsUsage = document.getElementById("processorsUsage");
 let memInfo = document.getElementById("memory-information");
-let storageInfo = document.getElementById("storage-information")
+let storageInfo = document.getElementById("storage-information");
 let oldInfo = 0;
 
-
-function toGB(val)
-{
-  return  Math.round(val / (1024 * 1024 * 1024));
+function toGB(val) {
+  return Math.round(val / (1024 * 1024 * 1024));
 }
-
 
 function updateCpuUsage(info) {
   let txt = ``;
   info.processors.forEach((processor, i) => {
     if (oldInfo) {
       let oldUsage = oldInfo.processors[i].usage;
-      usedInPercentage = Math.floor(((processor.usage.kernel + processor.usage.user) - (oldUsage.kernel + oldUsage.user)) / (processor.usage.total - oldUsage.total) * 100);
-
-  } else {
-      usedInPercentage = Math.floor((processor.usage.kernel + processor.usage.user) / processor.usage.total * 100);
-  }
+      usedInPercentage = Math.floor(
+        ((processor.usage.kernel +
+          processor.usage.user -
+          (oldUsage.kernel + oldUsage.user)) /
+          (processor.usage.total - oldUsage.total)) *
+          100
+      );
+    } else {
+      usedInPercentage = Math.floor(
+        ((processor.usage.kernel + processor.usage.user) /
+          processor.usage.total) *
+          100
+      );
+    }
     // let usedInPercentage = Math.floor(
     //   ((processor.usage.kernel + processor.usage.user) /
     //     processor.usage.total) *
@@ -46,39 +52,52 @@ function getCpuInformation() {
     cpuInfo.innerHTML = cpuSection;
     // updateCpuUsage(info);
     setInterval(() => {
-      chrome.system.cpu.getInfo(info => updateCpuUsage(info))
-    }, 1000)
+      chrome.system.cpu.getInfo((info) => updateCpuUsage(info));
+    }, 1000);
   });
 }
 
-function getMemUsage() {
-  chrome.system.memory.getInfo(function (info) {
-    let capacityInGB = toGB(info.capacity);
-    let availableCapacityInGB = toGB(info.availableCapacity); 
-    let memSection = `
+function getMemUsage(info) {
+  var capacity, used, usedInPercentage;
+  capacity = info.capacity / 1.074e9;
+  used = info.availableCapacity / 1.074e9;
+  leftInPercentage = Math.round((used / capacity) * 100);
+  usedInPercentage = 100 - leftInPercentage;
+
+  return {
+    used: usedInPercentage,
+    left: leftInPercentage,
+  };
+}
+
+function showMemUsage() {
+  setInterval(() => {
+    chrome.system.memory.getInfo(function (info) {
+      let usedCapacity = getMemUsage(info).used;
+      let capacityInGB = toGB(info.capacity);
+      let memSection = `
     <p> <b>Capacity: </b>  ${capacityInGB} GB </p>
-    <p> <b>Used Capacity: </b>  ${capacityInGB - availableCapacityInGB} GB </p>
-    <p> <b>Availbe Capacity: </b>  ${availableCapacityInGB} GB </p>
+    <p> <b>Used Capacity: </b>  ${usedCapacity} % </p>
     `;
-    memInfo.innerHTML = memSection;
-  });
+      memInfo.innerHTML = memSection;
+    });
+  }, 1000);
 }
 
-function getStorageUsage() 
-{
-  let txt = '';
-  chrome.system.storage.getInfo(function(info) {
-    info.forEach(storage => {
+function getStorageUsage() {
+  let txt = "";
+  chrome.system.storage.getInfo(function (info) {
+    info.forEach((storage) => {
       let capacity = toGB(storage.capacity);
       txt += `
         <p> <b> name: ${storage.name} (${storage.type}) </b>
         <p> <b>Capacity: </b>  ${capacity} GB </p>
       `;
-    })
+    });
     storageInfo.innerHTML = txt;
-  })
+  });
 }
 
 getCpuInformation();
-getMemUsage();
+showMemUsage();
 getStorageUsage();
